d419f103413aa9df69ca73549f32cf8f7b1860f1
mwangiderrick <mwangiderrick638@gmail.com>
2022-10-22 16:01:08 -0700

    woiye

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+void positive_or_negative(int i_);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int days, int year);
+int convert_day(int month, int day);
+
+#endif
